271239f3fb8c8862f8bd9612eadd09602959b549
ibaselyous <ibaseandroid@gmail.com>
2015-10-10 10:11:36 +0200

    updating list working fine

--- app/src/main/java/baselyous/com/copticsmedia/mediaTasks/tasks/factory/MediaTaskFactory.java
+++ app/src/main/java/baselyous/com/copticsmedia/mediaTasks/tasks/factory/MediaTaskFactory.java
@@ -2,12 +2,12 @@ package baselyous.com.copticsmedia.mediaTasks.tasks.factory;
 
 import baselyous.com.copticsmedia.mediaTasks.MediaDetailActivity;
 import baselyous.com.copticsmedia.mediaTasks.MediaDetailFragment;
-import baselyous.com.copticsmedia.mediaTasks.tasks.Agpeya.AgpeyaDetailActivity;
-import baselyous.com.copticsmedia.mediaTasks.tasks.Agpeya.AgpeyaFragment;
-import baselyous.com.copticsmedia.mediaTasks.tasks.Ebsalmodia.EbsalmodiaDetailActivity;
-import baselyous.com.copticsmedia.mediaTasks.tasks.Ebsalmodia.EbsalmodiaFragment;
-import baselyous.com.copticsmedia.mediaTasks.tasks.Kholagy.KholagyDetailActivity;
-import baselyous.com.copticsmedia.mediaTasks.tasks.Kholagy.KholagyFragment;
+import baselyous.com.copticsmedia.mediaTasks.tasks.agpeya.AgpeyaDetailActivity;
+import baselyous.com.copticsmedia.mediaTasks.tasks.agpeya.AgpeyaFragment;
+import baselyous.com.copticsmedia.mediaTasks.tasks.ebsalmodia.EbsalmodiaDetailActivity;
+import baselyous.com.copticsmedia.mediaTasks.tasks.ebsalmodia.EbsalmodiaFragment;
+import baselyous.com.copticsmedia.mediaTasks.tasks.kholagy.KholagyDetailActivity;
+import baselyous.com.copticsmedia.mediaTasks.tasks.kholagy.KholagyFragment;
 
 /**
  * Created by Ihab Baselyous on 03.10.2015.
